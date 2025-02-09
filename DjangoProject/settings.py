1764bb4888d47fe3f82cfe4b67689edfb3cc95a4
Slava <chempion2006vtf@gmail.com>
2025-02-09 12:56:45 +0300

    kaka

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -142,8 +142,8 @@ USE_TZ = True
 import os
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"  # Каталог, куда collectstatic будет собирать файлы
-STATICFILES_DIRS = []  # Должен быть пустым, иначе будет ошибка
+STATIC_ROOT = BASE_DIR / "staticfiles"  # Финальная папка для статики
+STATICFILES_DIRS = []  # Должен быть пустым
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
