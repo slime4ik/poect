1c170847eadd7c918de054818994c94e946c77d9
Slava <chempion2006vtf@gmail.com>
2025-02-09 13:21:30 +0300

    kaka

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -139,7 +139,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-import os
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"  # Финальная папка для статики
@@ -306,7 +305,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-import os
+
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
--- DjangoProject/wsgi.py
+++ DjangoProject/wsgi.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pythonProject1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoProject.settings')
 
 application = get_wsgi_application()
 
