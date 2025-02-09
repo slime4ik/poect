8bb0901bba1d638995402cce2b5c3a359c20abaf
Slava <chempion2006vtf@gmail.com>
2025-02-09 13:09:07 +0300

    aaa

--- DjangoProject/wsgi.py
+++ DjangoProject/wsgi.py
@@ -10,14 +10,10 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 # wsgi.py
 
 
-
-from waitress import serve
-from django.core.wsgi import get_wsgi_application
 import os
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoProject.settings')
 
 application = get_wsgi_application()
 
-if __name__ == "__main__":
-    serve(application, host='0.0.0.0', port=8000)
